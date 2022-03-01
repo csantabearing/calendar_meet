72aa9512188d3ec49f1ccea0d1b4ae49aef3a8dc
csantabearing <camilo@bearing.ai>
2022-03-01 15:24:12 -0500

    Calendar

--- schedule_events.py
+++ schedule_events.py
@@ -1,5 +1,8 @@
 from gcsa.google_calendar import GoogleCalendar
+import sched, time
 import webbrowser
+s = sched.scheduler(time.time, time.sleep)
+calendar = GoogleCalendar('camilo@bearing.ai')
 
 
 def open_event(code):
@@ -10,3 +13,14 @@ def open_event(code):
 def close_chrome():
     os.system("echo 'standby 0' | cec-client -s -d 1")
     os.system('pkill -o chromium-browser')
+
+
+while True:
+    for event in calendar:
+        if event.conference_solution:
+            if event.conference_solution['solution_type'] == 'hangoutsMeet':
+                s.enterabs(event.start,
+                           1,
+                           open_event,
+                           kwargs={'code': event.conference_solution['conference_id']})
+        s.run(False)
